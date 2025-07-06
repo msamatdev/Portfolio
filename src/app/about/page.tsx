3bb27631e3b7521e7ceab84e6d88aa0f8a930fa6
msamatdev <mathis.samat@gmail.com>
2025-07-06 19:44:12 +0200

    About page more translations

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -7,87 +7,87 @@ import Pictures from "@/components/about/Pictures";
 import { Images } from "lucide-react";
 import { useTranslations } from "next-intl";
 
-const Skills = [
-  {
-    name: "Python",
-    icon: "python",
-    level: "Average • Learnt mostly at university",
-    id: 1,
-  },
-  {
-    name: "Java",
-    icon: "java",
-    level: "Average • Learning it at university",
-    id: 2,
-  },
-  {
-    name: "Next.js",
-    icon: "nextjs",
-    level: "Good at Frontend • Learnt by myself",
-    id: 3,
-  },
-  {
-    name: "JavaScript, Typescript",
-    icon: "javascript",
-    level: "Correct • Learnt mostly by myself",
-    id: 4,
-  },
-  {
-    name: "HTML 5",
-    icon: "html",
-    level: "Good • Learnt by myself",
-    id: 5,
-  },
-  {
-    name: "CSS, Tailwind",
-    icon: "tailwind",
-    level: "Good • Learnt on my own",
-    id: 6,
-  },
-  {
-    name: "PostgreSQL",
-    icon: "postgresql",
-    level: "Beginner • Learnt at university",
-    id: 7,
-  },
-  {
-    name: "C",
-    icon: "c",
-    level: "Beginner • Learning it at school",
-    id: 8,
-  },
-  {
-    name: "Git",
-    icon: "git",
-    level: "Average • Learnt by myself",
-    id: 9,
-  },
-  {
-    name: "Docker",
-    icon: "docker",
-    level: "Average • Learnt by myself",
-    id: 10,
-  },
-  {
-    name: "Bash",
-    icon: "bash",
-    level: "Average • Learnt at school",
-    id: 11,
-  },
-  {
-    name: "Linux",
-    icon: "linux",
-    level: "Average • Learnt mostly by myself",
-    id: 12,
-  },
-];
-
 export const metadata: Metadata = {
   title: "About",
 };
 
 export default function About() {
   const t = useTranslations("AboutPage");
+
+  const Skills = [
+    {
+      name: "Python",
+      icon: "python",
+      level: `${t('tools.good')} • ${t('tools.selfTaught')}`,
+      id: 1,
+    },
+    {
+      name: "Java",
+      icon: "java",
+      level: `${t('tools.average')} • ${t('tools.learnAtUniv')}`,
+      id: 2,
+    },
+    {
+      name: "Next.js",
+      icon: "nextjs",
+      level: `${t('tools.good')} • ${t('tools.selfTaught')}`,
+      id: 3,
+    },
+    {
+      name: "JavaScript, Typescript",
+      icon: "javascript",
+      level: `${t('tools.average')} • ${t('tools.selfTaught')}`,
+      id: 4,
+    },
+    {
+      name: "HTML 5",
+      icon: "html",
+      level: `${t('tools.good')} • ${t('tools.mostlySelfTaught')}`,
+      id: 5,
+    },
+    {
+      name: "CSS, Tailwind",
+      icon: "tailwind",
+      level: `${t('tools.good')} • ${t('tools.selfTaught')}`,
+      id: 6,
+    },
+    {
+      name: "PostgreSQL",
+      icon: "postgresql",
+      level: `${t('tools.average')} • ${t('tools.learnAtUniv')}`,
+      id: 7,
+    },
+    {
+      name: "C",
+      icon: "c",
+      level: `${t('tools.beginner')} • ${t('tools.learnAtUniv')}`,
+      id: 8,
+    },
+    {
+      name: "Git",
+      icon: "git",
+      level: `${t('tools.average')} • ${t('tools.mostlySelfTaught')}`,
+      id: 9,
+    },
+    {
+      name: "Docker",
+      icon: "docker",
+      level: `${t('tools.average')} • ${t('tools.selfTaught')}`,
+      id: 10,
+    },
+    {
+      name: "Bash",
+      icon: "bash",
+      level: `${t('tools.average')} • ${t('tools.learnAtUniv')}`,
+      id: 11,
+    },
+    {
+      name: "Linux",
+      icon: "linux",
+      level: `${t('tools.average')} • ${t('tools.mostlySelfTaught')}`,
+      id: 12,
+    },
+  ];
 
   return (
     <MaxWidthWrapper>
@@ -116,10 +116,10 @@ export default function About() {
           {t("tools.title")}
         </h3>
         <Separator />
-        <div className="grid place-items-center mt-16 mx-6 gap-8 lg:grid-cols-3 md:grid-cols-2 grid-cols-1">
+        <div className="grid place-items-center mt-16 mx-6 gap-10 lg:grid-cols-3 md:grid-cols-2 grid-cols-1">
           {Skills.map((skill) => (
             <div
-              className="dark:bg-muted/15 border-2 border-muted shadow-md rounded-md w-full h-full p-6 flex flex-col items-center justify-center"
+              className="dark:bg-muted/15 border-2 border-muted shadow-md rounded-md w-full h-full px-2 py-8 flex flex-col items-center justify-center"
               key={skill.id}
             >
               <Image
@@ -138,38 +138,30 @@ export default function About() {
       </div>
       <div className="mt-44 px-12 mb-12">
         <h1 className="text-center text-4xl font-bold tracking-tight text-foreground sm:text-5xl">
-          { t('outsideUniversity.title') }
+          {t("outsideUniversity.title")}
         </h1>
         <div className="grid lg:grid-cols-2 grid-cols-1 place-items-center mt-12 lg:mt-24 lg:px-30 gap-12">
           <div className="text-lg">
-            <p>
-              { t('outsideUniversity.p1') }
-            </p>
+            <p>{t("outsideUniversity.p1")}</p>
             <p className="mt-4">
-              { t('outsideUniversity.p2.p1') }
+              {t("outsideUniversity.p2.p1")}
               <Link
                 href="https://youtu.be/MTAOKLT-zNA?t=10"
                 className="decoration-dashed underline underline-offset-4 decoration-1"
                 target="_blank"
               >
-                { t('outsideUniversity.p2.link') }
+                {t("outsideUniversity.p2.link")}
               </Link>
-              { t('outsideUniversity.p2.p2') }
+              {t("outsideUniversity.p2.p2")}
             </p>
-            <p className="mt-4">
-              { t('outsideUniversity.p3') }
-            </p>
+            <p className="mt-4">{t("outsideUniversity.p3")}</p>
           </div>
           <Pictures />
         </div>
         <div className="grid lg:grid-cols-2 grid-cols-1 place-items-center mt-12 lg:mt-24 lg:px-30 gap-12">
-          <Image 
-            
-          />
+          <Image />
 
-          <p>
-            { t('outsideUniversity.p4') }
-          </p>
+          <p>{t("outsideUniversity.p4")}</p>
         </div>
       </div>
     </MaxWidthWrapper>
